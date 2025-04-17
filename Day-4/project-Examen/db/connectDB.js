e55a2a3229801c3d7c63439f11c03aebaf9f073b
orlinelove <orlinelove@debian.ICT.com>
2025-04-17 21:46:29 +0200

    création d'un fichier environnement pour stocker les variables sensibles

--- Day-4/project-Examen/db/connectDB.js
+++ Day-4/project-Examen/db/connectDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = () => {
   mongoose
-    .connect("mongodb://localhost:27017/auth-api")
+    .connect("mongodb://localhost:27017/exam-db")
     .then(() => console.log("Connected"))
     .catch((err) => console.error("connection error: ", err));
 };
