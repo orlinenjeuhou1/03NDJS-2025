65bd0a86ff0828812e8db967f1ef191c449ea93a
orlinelove <orlinelove@debian.ICT.com>
2025-04-18 11:45:18 +0200

    Dernier commit apres mes tests potsman et ma dockerisation

--- Day-4/project-Examen/db/connectDB.js
+++ Day-4/project-Examen/db/connectDB.js
@@ -1,10 +1,18 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+// Charger les variables d'environnement depuis le fichier .env
+dotenv.config();
 
 const connectDB = () => {
-    mongoose
-    .connect("mongodb://host.docker.internal:27017/exam-db") 
-    .then(() => console.log("Connected"))
-    .catch((err) => console.error("connection error: ", err));
+  mongoose
+    .connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+    .then(() => console.log("✅ Connexion MongoDB Atlas réussie"))
+    .catch((err) => console.error("❌ Erreur de connexion MongoDB Atlas :", err));  // Correction ici
 };
 
 export default connectDB;
+
